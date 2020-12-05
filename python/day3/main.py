d44930a5dcfc266da6eb7571496b61d3a47cfa6a
Amar1729 <amar.paul16@gmail.com>
2020-12-04 19:44:41 -0500

    d3p2

--- python/day3/main.py
+++ python/day3/main.py
@@ -1,17 +1,28 @@
 #! /usr/bin/env python3
 
 import sys
+import functools
 
 
-def calc(content) -> int:
+SLOPES = [
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2),
+]
+
+
+def calc(content, slope) -> int:
     nature = [line.strip() for line in content if line.strip()]
 
+    x, y = slope
     width, height = len(nature[0]), len(nature)
 
     pos = 0
     trees = 0
 
-    for i in range(height):
+    for i in range(0, height, y):
         if i > 0:
             if nature[i][pos] == "#":
                 # nature[i][pos] = "X"
@@ -21,7 +32,7 @@ def calc(content) -> int:
                 # nature[i][pos] = "O"
                 nature[i] = nature[i][:pos] + "O" + nature[i][pos + 1:]
 
-        pos = (pos + 3) % width
+        pos = (pos + x) % width
 
     return trees
 
@@ -30,7 +41,8 @@ def main():
     with open(sys.argv[1], "r") as f:
         content = f.readlines()
 
-    result = calc(content)
+    # result = calc(content)
+    result = functools.reduce(lambda x, y: x * y, [calc(content, s) for s in SLOPES])
     print(result)
 
 
