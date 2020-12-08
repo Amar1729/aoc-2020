98e6ebd0c2feba6a0e5f7d3885169532c9387340
Amar1729 <amar.paul16@gmail.com>
2020-12-08 00:40:26 -0500

    d8p2

--- python/day8/main.py
+++ python/day8/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import copy
 import sys
 
 
@@ -9,10 +10,13 @@ def p1(content):
     states = set()
 
     while True:
-        inst, offset = content[counter].split(" ")
+        try:
+            inst, offset = content[counter].split(" ")
+        except IndexError:
+            return accumulator, True
 
         if counter in states:
-            return accumulator
+            return accumulator, False
 
         if inst == "nop":
             states.add(counter)
@@ -27,14 +31,25 @@ def p1(content):
 
 
 def p2(content):
-    pass
+    for bad_inst, _ in filter(lambda i: i[1].split(" ")[0] in ["nop", "jmp"], enumerate(content)):
+        modified = copy.copy(content)
+        if "nop" in modified[bad_inst]:
+            modified[bad_inst] = modified[bad_inst].replace("nop", "jmp")
+        elif "jmp" in modified[bad_inst]:
+            modified[bad_inst] = modified[bad_inst].replace("jmp", "nop")
+
+        result, status = p1(modified)
+        if status:
+            return result
+
+    raise ValueError("terminating program not found?")
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
--- python/skeleton.py
+++ python/skeleton.py
@@ -3,16 +3,19 @@
 import sys
 
 
-def calc():
-    return
+def p1(content):
+    pass
+
+
+def p2(content):
+    pass
 
 
 def main():
-    with open(sys.argv[1], "r") as f:
-        content = f.readlines()
+    content = [l.rstrip("\n") for l in sys.stdin.read()]
 
-    result = calc(content)
-    print(result)
+    print(p1(content))
+    # print(p2(content))
 
 
 if __name__ == "__main__":
