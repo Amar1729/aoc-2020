128512a1d1ebd0151776f0d76ba3c5d81aadb59f
Amar1729 <amar.paul16@gmail.com>
2020-12-09 00:50:20 -0500

    d9p2

--- python/day9/main.py
+++ python/day9/main.py
@@ -10,28 +10,54 @@ def p1(content):
     numbers = list(map(int, content))
     d = collections.deque(numbers[:WIDTH], maxlen=WIDTH)
 
-    def check_correct(j) -> False:
+    def check_correct(j) -> bool:
         for x in d:
             if j - x in d:
                 return True
         return False
 
-    for n in numbers[WIDTH:]:
+    for idx, n in enumerate(numbers[WIDTH:]):
         if check_correct(n):
             d.append(n)
         else:
-            return n
+            return idx, n
 
 
 def p2(content):
-    pass
+    idx, total = p1(content)
+    numbers = list(map(int, content[:idx]))
+
+    # i and j are lower/upper bounds for a contiguous range in our valid numbers
+    i = 0
+    j = 1
+
+    curr_sum = numbers[0] + numbers[1]
+    while True:
+        # optimize this list access out (manually keep track of additions/subtractions)
+        # curr_sum = sum(numbers[i:j + 1])
+
+        if curr_sum == total:
+            print(f"Range: {i} - {j}")
+            print(numbers[i:j + 1])
+            answer = sorted(numbers[i:j + 1])
+            return answer[0] + answer[-1]
+        elif curr_sum < total:
+            # too small: range should be increased
+            j += 1
+            curr_sum += numbers[j]
+        elif curr_sum > total:
+            # too large: range should be decreased
+            curr_sum -= numbers[i]
+            i += 1
+
+    return total
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
