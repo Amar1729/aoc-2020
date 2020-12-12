4840c4ec4309fc1314ed565aa00cbddb32386b3a
Amar1729 <amar.paul16@gmail.com>
2020-12-12 17:24:50 -0500

    d10p2

--- python/day10/main.py
+++ python/day10/main.py
@@ -3,6 +3,8 @@
 import itertools
 import sys
 
+from typing import Dict
+
 
 def pairwise(itr):
     a, b = itertools.tee(iter(itr))
@@ -35,15 +37,40 @@ def p1(content) -> int:
     return count_1 * count_3
 
 
-def p2(content):
-    pass
+def p2(content) -> int:
+    adapters = sorted(map(int, content))
+    adapters = [0] + adapters
+
+    # try calculating from the end first
+    memo: Dict[int, int] = {}
+
+    def recurse(i: int, prev) -> int:
+        if i < 0:
+            return 0
+
+        if prev - adapters[i] > 3:
+            return 0
+
+        if i == 0:
+            return 1
+
+        # don't redo work - use memoization !
+        if i in memo:
+            return memo[i]
+
+        prev = adapters[i]
+        total = sum(recurse(i - x, prev) for x in reversed(range(1, 4)))
+        memo[i] = total
+        return total
+
+    return recurse(len(adapters) - 1, adapters[-1] + 3)
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
