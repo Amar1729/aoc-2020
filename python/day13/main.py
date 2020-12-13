9840d1e5d6869a920e8e4d2cf78f2384db26d649
Amar1729 <amar.paul16@gmail.com>
2020-12-13 03:28:45 -0500

    d13p2

--- python/day13/main.py
+++ python/day13/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
+from functools import reduce
 
 
 def p1(content) -> int:
@@ -16,15 +17,43 @@ def p1(content) -> int:
         timestamp += 1
 
 
-def p2(content):
-    pass
+def e_gcd(a, b):
+    ra, r = a, b
+    sa, s = 1, 0
+    ta, t = 0, 1
+
+    while r != 0:
+        q = ra // r
+        ra, r = r, ra - q * r
+        sa, s = s, sa - q * s
+        ta, t = t, ta - q * t
+
+    return sa, ta
+
+
+def p2(content) -> int:
+    buses = filter(
+        lambda e: isinstance(e[1], int),
+        map(
+            lambda i: i if i[1] == "x" else (-i[0], int(i[1])),
+            enumerate(content[1].split(",")),
+        ),
+    )
+
+    def _fold(a1, n1, a2, n2):
+        m1, m2 = e_gcd(n1, n2)
+        x = (a1 % n1) * m2 * n2 + (a2 % n2) * m1 * n1
+        N = n1 * n2
+        return x % N, N
+
+    return reduce(lambda x, y: _fold(*x, *y), buses)[0]
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
