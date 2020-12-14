d2766663a25382ef7eb280b7ab0da6e82443bb1f
Amar1729 <amar.paul16@gmail.com>
2020-12-14 01:00:49 -0500

    d14p2

--- python/day14/main.py
+++ python/day14/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import collections
+import itertools
 import re
 import sys
 
@@ -9,6 +10,21 @@ def bitmask(value: str, mask: str) -> int:
     return int(
         "".join(map(lambda x: x[0] if x[1] == "X" else x[1], zip(value, mask))), 2
     )
+
+
+def bitmask_p2(reg: str, mask: str):
+    masked = "".join(map(lambda x: x[0] if x[1] == "0" else x[1], zip(reg, mask)))
+
+    floating = list(map(lambda x: x[0], filter(lambda x: x[1] == 'X', enumerate(masked))))
+
+    if floating:
+        for digits in itertools.product([0, 1], repeat=len(floating)):
+            s = masked
+            for digit in digits:
+                s = s.replace("X", str(digit), 1)
+            yield s
+    else:
+        yield masked
 
 
 def p1(content) -> int:
@@ -30,14 +46,28 @@ def p1(content) -> int:
 
 
 def p2(content):
-    pass
+    mem = collections.defaultdict(int)
+
+    mask = ""
+
+    for line in content:
+        instruction, value = line.split(" = ")
+
+        if instruction == "mask":
+            mask = value
+        else:
+            reg = int(re.search(r"\d+", instruction).group())
+            for masked_reg in bitmask_p2(f"{reg:0>36b}", mask):
+                mem[int(masked_reg, 2)] = int(value)
+
+    return sum(mem.values())
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
