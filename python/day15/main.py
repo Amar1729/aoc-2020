bf43269e3be95b3f25d1e2f0ce8698cca6aa38df
Amar1729 <amar.paul16@gmail.com>
2020-12-15 00:37:52 -0500

    d15p1

---
+++ python/day15/main.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+import collections
+import sys
+
+
+def p1(content) -> int:
+    seed = list(map(int, content[0].split(",")))
+
+    d = collections.defaultdict(list)
+
+    prev = 0
+    for idx, n in enumerate(seed):
+        d[n].append(idx + 1)
+        prev = n
+
+    for idx in range(idx + 1, 2020):
+        prev = 0 if len(d[prev]) < 2 else d[prev][-1] - d[prev][-2]
+        d[prev].append(idx + 1)
+
+    return prev
+
+
+def p2(content):
+    pass
+
+
+def main():
+    content = sys.stdin.read().rstrip().split("\n")
+
+    print(p1(content))
+    # print(p2(content))
+
+
+if __name__ == "__main__":
+    main()
