75c23a6f45865b86051ed22204d921c46d0874f0
Amar1729 <amar.paul16@gmail.com>
2020-12-26 02:06:11 -0500

    d23p2

--- python/day23/main.py
+++ python/day23/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import itertools
 import sys
 
 
@@ -40,15 +41,80 @@ def p1(content):
     return st
 
 
-def p2(content):
-    pass
+def p2(content) -> int:
+    # ### ---- initialization work ----
+
+    # cups: cups[label] = label of next cup (clockwise)
+    cups = [0] * 1_000_001
+
+    # similar to `cups`, but for finding the previous (lower label) cup
+    prev_cups = [0] * 1_000_001
+
+    cup_values = list(map(int, list(content[0])))
+    curr = cup_values[0]
+
+    a, b = itertools.tee(
+        itertools.chain(
+            cup_values,
+            range(len(cup_values) + 1, 1_000_001),
+        )
+    )
+    next(b)
+    for idx, (c1, c2) in enumerate(zip(a, b)):
+        cups[c1] = c2
+        if idx > len(cup_values):
+            prev_cups[idx] = idx - 2
+    cups[c2] = cup_values[0]
+
+    for _ in range(2):
+        idx += 1
+        prev_cups[idx] = idx - 2
+
+    for i in itertools.chain(cups[1:len(cup_values) + 1], [cups[-1]]):
+        prev_cups[i] = 10 ** 6 - 1 if i == 1 else cups.index(i - 1)
+
+    # ### ---- helper functions ----
+
+    def find_target(c: int) -> int:
+        target_idx = prev_cups[c]
+        while cups[target_idx] in to_del:
+            target_idx = prev_cups[cups[target_idx]]
+        return target_idx
+
+    def safe_inc(x: int) -> int:
+        return x + 1 if x < 10 ** 6 else 1
+
+    # ### ---- do stuff ----
+
+    for counter in range(10 ** 7):
+        to_del = [cups[curr]]
+        for _ in range(2):
+            to_del.append(cups[to_del[-1]])
+
+        target = find_target(curr)
+
+        x, y, z, w = cups[curr], cups[to_del[-1]], cups[cups[target]], cups[target]
+
+        # modify our index -> next cup mapping
+        cups[curr] = y  # curr -> after-last-pick
+        cups[to_del[-1]] = z  # last-pick -> after-target
+        cups[w] = x  # target -> first-pick
+
+        # modify index -> prev cup mapping
+        prev_cups[safe_inc(y)] = curr
+        prev_cups[safe_inc(z)] = to_del[-1]
+        prev_cups[safe_inc(x)] = w
+
+        curr = cups[curr]
+
+    return cups[1] * cups[cups[1]]
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
