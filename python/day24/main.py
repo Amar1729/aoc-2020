4f03561b7f64d28d1585469712bd614153811af3
Amar1729 <amar.paul16@gmail.com>
2020-12-27 00:50:32 -0500

    d24p2

--- python/day24/main.py
+++ python/day24/main.py
@@ -14,7 +14,7 @@ def parse_directions(s):
     return dirs
 
 
-def p1(content):
+def p1(content, p2=False):
     dirs = list(map(parse_directions, content))
     points = set()
 
@@ -40,18 +40,55 @@ def p1(content):
             # print(f"adding: {p}")
             points.add(p)
 
-    return len(points)
+    # return the points set for our p2 work
+    return len(points) if not p2 else points
 
 
 def p2(content):
-    pass
+    points = p1(content, True)
+
+    def yield_adj(p):
+        """ yield all adjacent points to p """
+        yield (p[0] + 1, p[1])
+        yield (p[0], p[1] - 1)
+        yield (p[0] - 1, p[1] - 1)
+        yield (p[0] - 1, p[1])
+        yield (p[0] + 1, p[1] + 1)
+        yield (p[0], p[1] + 1)
+
+    def check_adj(p) -> int:
+        """ return number of adjacent black tiles """
+        return sum(_p in points for _p in yield_adj(p))
+
+    for i in range(100):
+        new_points = set()
+        already_white = set()
+
+        for point in points:
+            adj_black = 0
+            for _point in yield_adj(point):
+                if _point in points:
+                    adj_black += 1
+                else:
+                    if _point not in already_white:
+                        already_white.add(_point)
+                        if check_adj(_point) == 2:
+                            new_points.add(_point)
+
+            if adj_black in [1, 2]:
+                new_points.add(point)
+
+        points = new_points
+        # print(f"Day {i+1}: {len(points)}")
+
+    return len(points)
 
 
 def main():
     content = sys.stdin.read().rstrip().split("\n")
 
-    print(p1(content))
-    # print(p2(content))
+    # print(p1(content))
+    print(p2(content))
 
 
 if __name__ == "__main__":
